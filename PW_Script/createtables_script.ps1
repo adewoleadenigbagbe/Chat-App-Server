f1c397859154dd974fb063ca994a17857817c137
Wolecharles <cadenigbagbe@appzonegroup.com>
2019-05-29 09:29:15 +0100

    10th commit

--- PW_Script/createtables_script.ps1
+++ PW_Script/createtables_script.ps1
@@ -1,1 +1,1 @@
-﻿Invoke-sqlcmd -ServerInstance 'devAnsibleEnv\SQLEXPRESS' -Database ChatDB -InputFile 'C:\GitRepo.CompiledCode\Chat-App-Server\PW_Script\createtables_script.ps1'
+﻿Invoke-sqlcmd -ServerInstance 'devAnsibleEnv\SQLEXPRESS' -Database 'ChatDB' -InputFile 'C:\GitRepo.CompiledCode\Chat-App-Server\PW_Script\createtables_script.ps1'
